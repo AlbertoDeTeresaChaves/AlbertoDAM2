36c98f880563596b195f755241931ac1bc21e364
alberto <alberto.trabajo.es@gmail.com>
2024-01-10 23:16:52 +0100

    Correccion del index.javaScript

--- javaScript/index_javaScript.js
+++ javaScript/index_javaScript.js
@@ -2,16 +2,20 @@ function redirigir(action) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if(username=="" || password==""){
-        alert("Debe poner usuario y contraseña")
-    }else{
+    
     if (action === 'login') {
+        if(username=="" || password==""){
+            alert("Debe poner usuario y contraseña")
+        }else{
         console.log('Haciendo login con ' + username + ' y ' + password);
         window.open('../AlbertoDAM2/home.html')
-    } else if (action === 'register') {
+        }
+    }
+    
+    if(action === 'register') {
         window.open('../AlbertoDAM2/register.html')
     }
-    }
 }
+
 
 
