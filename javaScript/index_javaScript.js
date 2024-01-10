5a39010472e97af628968d194f0c498cbbb101ba
alberto <alberto.trabajo.es@gmail.com>
2024-01-10 23:12:48 +0100

    Avance con el register y enter

--- javaScript/index_javaScript.js
+++ javaScript/index_javaScript.js
@@ -2,11 +2,15 @@ function redirigir(action) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
+    if(username=="" || password==""){
+        alert("Debe poner usuario y contraseña")
+    }else{
     if (action === 'login') {
         console.log('Haciendo login con ' + username + ' y ' + password);
-        window.location.href = '../AlbertoDAM2/home.html';
+        window.open('../AlbertoDAM2/home.html')
     } else if (action === 'register') {
-        window.location.href = '../AlbertoDAM2/register.html';
+        window.open('../AlbertoDAM2/register.html')
+    }
     }
 }
 
